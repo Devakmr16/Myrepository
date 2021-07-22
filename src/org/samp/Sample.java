3ff0f2cae0a2d300308151bfb4a9dfbb0d7c5b70
Devakmr16 <devakmr16@gmail.com>
2021-07-22 13:57:27 +0530

    move to master

---
+++ src/org/samp/Sample.java
@@ -0,0 +1,17 @@
+package org.samp;
+
+public class Sample {
+
+	private void savings() {
+		System.out.println("saving");
+
+	}
+	
+	private void deposit() {
+		System.out.println("deposit");
+	}
+	
+	private void fixed() {
+		System.out.println("fixed");
+	}
+}
